02852d05bdeb555e6dc6082aedca07d3ccd47ac8
Pingfan Hu <pingfan0727@gmail.com>
2026-04-14 22:36:39 -0400

    feat: integrate Sparkle 2.x for auto-update
    - Add Sparkle 2.9.1 as Swift Package dependency in project.yml
    - Initialize SPUStandardUpdaterController in AppDelegate
    - Add SUFeedURL and SUPublicEDKey to Info.plist
    - Update appcast.xml: switch enclosure to ZIP, add EdDSA signature for v0.1.0

--- MiniNotes/App/AppDelegate.swift
+++ MiniNotes/App/AppDelegate.swift
@@ -1,10 +1,13 @@
 import AppKit
+import Sparkle
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusBarController: StatusBarController?
+    private var updaterController: SPUStandardUpdaterController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusBarController = StatusBarController()
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
